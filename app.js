330e36aa7d297c3732fe93fe3f634db60a61a5ae
peters <petes726@gmail.com>
2024-07-05 14:54:42 -0400

    Implementing custom error catching 404 and 500 templates

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
 
@@ -42,6 +43,8 @@ app.get('/campgrounds/new', (req, res) => {
 app.post(
 	'/campgrounds',
 	catchAsync(async (req, res) => {
+		if (!req.body.campground)
+			throw new ExpressError('Invalid Campground Data', 400);
 		const campground = new Campground(req.body.campground);
 		await campground.save();
 		res.redirect(`/campgrounds/${campground._id}`);
@@ -85,8 +88,13 @@ app.delete(
 	})
 );
 
+app.all('*', (req, res, next) => {
+	next(new ExpressError('Page Not Found', 404));
+});
+
 app.use((err, req, res, next) => {
-	res.send('Something went wrong!');
+	const { statusCode = 500, message = 'Something went wrong' } = err;
+	res.status(statusCode).send(message);
 });
 
 app.listen(3000, () => {
