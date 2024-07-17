6be900833ccb472a863733b258029dcf12af1f68
peters <petes726@gmail.com>
2024-07-16 22:32:09 -0400

    Adding review creation and pushing to Campground array

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const { campgroundSchema } = require('./schemas');
-
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
+const Review = require('./models/review');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -97,6 +97,18 @@ app.delete(
 		const { id } = req.params;
 		await Campground.findByIdAndDelete(id);
 		res.redirect('/campgrounds');
+	})
+);
+
+app.post(
+	'/campgrounds/:id/reviews',
+	catchAsync(async (req, res) => {
+		const campground = await Campground.findById(req.params.id);
+		const review = new Review(req.body.review);
+		campground.reviews.push(review);
+		await review.save();
+		await campground.save();
+		res.redirect(`/campgrounds/${campground._id}`);
 	})
 );
 
